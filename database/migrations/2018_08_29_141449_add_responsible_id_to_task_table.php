28ad2814f03d6f23c9c23e9f7258b24f4db68a08
CodeDragon <hola@juliatrefas.com>
2018-08-31 09:28:03 +0200

    fixed migration

--- database/migrations/2018_08_29_141449_add_responsible_id_to_task_table.php
+++ database/migrations/2018_08_29_141449_add_responsible_id_to_task_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Task;
+use App\User;
 
 class AddResponsibleIdToTaskTable extends Migration
 {
